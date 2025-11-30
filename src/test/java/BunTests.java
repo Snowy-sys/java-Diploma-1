48b6bd6bd51c0350dc21a91f3d5775567401c546
Nikulin Maksim <maxveter2000@gmail.com>
2025-11-30 23:36:22 +0300

    Добавлены 2 теста для класса Bun, а также пакет с константами

--- src/test/java/BunTests.java
+++ src/test/java/BunTests.java
@@ -1,2 +1,31 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import praktikum.Bun;
+
+import static constants.bun.BunInt.PRICE_100;
+import static constants.bun.BunString.GREEN_BUN;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BunTests {
+
+    public Bun bun;
+
+    @BeforeEach
+    @DisplayName("Создание конструктора")
+    void constrictorCreation() {
+        bun = new Bun(GREEN_BUN, PRICE_100);
+    }
+
+    @Test
+    @DisplayName("Проверка возврата значений функции получения названия булочки")
+    void checkGetNameOfBun() {
+        assertEquals(GREEN_BUN, bun.getName());
+    }
+
+    @Test
+    @DisplayName("Проверка возврата значений функции получения цены булочки")
+    void checkGetPriceOfBun() {
+        assertEquals(PRICE_100, bun.getPrice());
+    }
 }
